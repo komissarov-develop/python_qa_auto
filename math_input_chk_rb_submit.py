cd358bd76d1f4cd43f743366a1fdab9707fe2322
komissarov-develop <44498798+komissarov-develop@users.noreply.github.com>
2020-08-26 22:30:17 +0300

    Add files via upload

---
+++ math_input_chk_rb_submit.py
@@ -0,0 +1,31 @@
+﻿import math
+import time
+from selenium import webdriver
+
+
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+    checkbox = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    checkbox.click()
+    radioButton = browser.find_element_by_css_selector("[value='robots']")
+    radioButton.click()
+    submit = browser.find_element_by_css_selector('button')
+    submit.click()
+
+finally:
+    time.sleep(30)
+
+    browser.quit()
+
+    
