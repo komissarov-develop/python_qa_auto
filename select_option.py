b5f0f7d66160e864e89c7ac743b35d3960e1d474
komissarov-develop <44498798+komissarov-develop@users.noreply.github.com>
2020-08-28 13:36:54 +0300

    Add files via upload

---
+++ select_option.py
@@ -0,0 +1,31 @@
+﻿import math
+import time
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+
+
+
+
+
+link = "http://suninjuly.github.io/selects1.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link) 
+    num1 = browser.find_element_by_id('num1')
+    x = num1.text
+    num2 = browser.find_element_by_id('num2')
+    y = num2.text
+    sum_el = int(x) + int(y)
+    sum_el = str(int(x)+int(y))
+    select = Select(browser.find_element_by_tag_name("select"))
+    select.select_by_value(sum_el)
+    submit = browser.find_element_by_css_selector('button')
+    submit.click()
+
+finally:
+    time.sleep(5)
+
+    browser.quit()
+
+    
