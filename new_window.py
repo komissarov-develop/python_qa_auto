523a57d8e7f4617c94fae2f61ec321c189bb64c0
komissarov-develop <44498798+komissarov-develop@users.noreply.github.com>
2020-09-01 14:39:15 +0300

    Add files via upload

---
+++ new_window.py
@@ -0,0 +1,39 @@
+﻿import math
+import time
+from selenium import webdriver
+
+
+
+
+
+try:
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    browser = webdriver.Chrome()
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser.get(link)
+    submit = browser.find_element_by_css_selector('button')
+    submit.click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    num = browser.find_element_by_id('input_value')
+    x = num.text
+    y = calc(x)
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+    submit = browser.find_element_by_css_selector('button')
+    submit.click()
+    
+
+finally:
+    time.sleep(10)
+
+    browser.quit()
+
+
+
+
+#browser.switch_to.window(window_name) - переход на новое окно
+#new_window = browser.window_handles[1]  - вы даёт имя второй вкладки
+#first_window = browser.window_handles[0] - имя текущей владки
+
---
+++ window_alert_accept.py
@@ -0,0 +1,49 @@
+﻿import math
+import time
+from selenium import webdriver
+
+
+
+
+
+try:
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    browser = webdriver.Chrome()
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser.get(link)
+    submit = browser.find_element_by_css_selector('button')
+    submit.click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    num = browser.find_element_by_id('input_value')
+    x = num.text
+    y = calc(x)
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+    submit = browser.find_element_by_css_selector('button')
+    submit.click()
+    
+    
+
+finally:
+    time.sleep(10)
+
+    browser.quit()
+
+
+
+
+#alert = browser.switch_to.alert
+#alert.accept() //принять алерт
+
+#alert = browser.switch_to.alert
+#alert_text = alert.text  //текст из алерт
+
+#confirm = browser.switch_to.alert
+#confirm.accept()
+#confirm.dismiss()
+
+#prompt = browser.switch_to.alert
+#prompt.send_keys("My answer")
+#prompt.accept()
